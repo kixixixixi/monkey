20141c86c9104922466da0a4389afc64bb424060
kixixixixi <kixixixixi@gmail.com>
2021-05-06 03:12:07 +0900

    Set api base url

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
 
-axios.defaults.baseURL = "http://localhost:3333/v1"
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3333/v1"
 
 export default axios
